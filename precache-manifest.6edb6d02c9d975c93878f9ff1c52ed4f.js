c6d24c79509c4f8c16f2a3520e8a007bc6832792
Denys Mohylin <dmohylin@bizico.com>
2019-07-14 01:26:14 +0300

    Updates

--- precache-manifest.6edb6d02c9d975c93878f9ff1c52ed4f.js
+++ precache-manifest.6edb6d02c9d975c93878f9ff1c52ed4f.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cffeff45ecd0074f22d35f60d0193019",
+    "revision": "7f4a9d9f6d1aec36bd08945e5442efd9",
     "url": "/vacation/index.html"
   },
   {
-    "revision": "398d2ce663910c3a9e3d",
+    "revision": "0a01cac6a7007b5d9ecd",
     "url": "/vacation/static/css/2.32bbf376.chunk.css"
   },
   {
-    "revision": "d9772c8b2b13e1093cd9",
+    "revision": "3e985ddc7ee43caed3e2",
     "url": "/vacation/static/css/main.498d002a.chunk.css"
   },
   {
-    "revision": "398d2ce663910c3a9e3d",
-    "url": "/vacation/static/js/2.1eaa787c.chunk.js"
+    "revision": "0a01cac6a7007b5d9ecd",
+    "url": "/vacation/static/js/2.8a66f125.chunk.js"
   },
   {
-    "revision": "d9772c8b2b13e1093cd9",
-    "url": "/vacation/static/js/main.cc7c7443.chunk.js"
+    "revision": "3e985ddc7ee43caed3e2",
+    "url": "/vacation/static/js/main.8c7d6c41.chunk.js"
   },
   {
     "revision": "891a2bcbd4e8a6f7de82",
     "url": "/vacation/static/js/runtime~main.ea516ffb.js"
+  },
+  {
+    "revision": "74b568c61396168efac404115c63c482",
+    "url": "/vacation/static/media/grebec-back.74b568c6.jpg"
   },
   {
     "revision": "c38ba5ee93bd9377b53ebf81c180c08d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vacation/precache-manifest.741f5f0b6b91ea1805437b6b7c461603.js"
+  "/vacation/precache-manifest.6edb6d02c9d975c93878f9ff1c52ed4f.js"
 );
 
 self.addEventListener('message', (event) => {
