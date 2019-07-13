7a258ebbcc15c709dc3dbe999cede876e1605d22
Denys Mohylin <dmohylin@bizico.com>
2019-07-13 22:45:29 +0300

    Updates

---
+++ precache-manifest.66f3f2151f4a39eba738930b0e9fb527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d940aa839ea249d54155199c060820c3",
+    "url": "/vacation/index.html"
+  },
+  {
+    "revision": "398d2ce663910c3a9e3d",
+    "url": "/vacation/static/css/2.32bbf376.chunk.css"
+  },
+  {
+    "revision": "40ba4cf45965b78a6b06",
+    "url": "/vacation/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "398d2ce663910c3a9e3d",
+    "url": "/vacation/static/js/2.1eaa787c.chunk.js"
+  },
+  {
+    "revision": "40ba4cf45965b78a6b06",
+    "url": "/vacation/static/js/main.6072fae4.chunk.js"
+  },
+  {
+    "revision": "891a2bcbd4e8a6f7de82",
+    "url": "/vacation/static/js/runtime~main.ea516ffb.js"
+  },
+  {
+    "revision": "c38ba5ee93bd9377b53ebf81c180c08d",
+    "url": "/vacation/static/media/grebec.c38ba5ee.jpg"
+  }
+]);
