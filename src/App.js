72a0f51b81cd6ed51fa9c78b0363495791ebbb11
Denys Mohylin <dmohylin_ext@oneviewhealthcare.com>
2019-12-22 21:13:36 +0200

    [feat] vacation vol. 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import moment from "moment";
 import grebec from "./grebec.jpg";
+import ship from "./pirate_boat.png";
+import vacation from "./vacation.jpg";
+import office from "./office.jpg";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 const humanizeUADate = daysNumber => {
@@ -17,9 +20,14 @@ const humanizeUADate = daysNumber => {
 
 function App() {
   const startDate = new Date();
-  const endDate = moment("2019-08-05T00:00:00.000Z").toDate();
+  const endDate = moment("2020-01-09T00:00:00.000Z").toDate();
   const diff = moment.duration(endDate - startDate).asDays();
   const diffFormatted = Math.ceil(diff);
+
+  const VACATION_DURATION = 17;
+
+  const daysArr = Array(VACATION_DURATION).fill("mock");
+
 
   return (
     <div className="container">
@@ -29,22 +37,48 @@ function App() {
             До повернення грєбца залишилось
             <br />
             <bold style={{ color: "blue" }}>
-              {humanizeUADate(diffFormatted)}
+              <i> {humanizeUADate(diffFormatted)}</i>
             </bold>
           </h1>
-          <div className="p-4">
+        </div>
+      </div>
+      <div className="row">
+        {daysArr.map((_, index) => (
+          <div className="col-sm-2 col-4">
             <img
-              width="40%"
               style={{
-                borderRadius: ".25em",
-                minWidth: "280px",
-                maxWidth: "560px"
+                width: "90px",
+                transform: "rotateY(180deg)",
+                display: "block",
+                visibility: `${
+                  index + 1 === VACATION_DURATION - diffFormatted ? "visible" : "hidden"
+                }`,
+                margin: '10px auto'
               }}
-              src={grebec}
+              src={ship}
               alt=""
             />
+            <div
+              style={{
+                width: "45px",
+                height: "45px",
+                borderRadius: "50%",
+                backgroundColor: `${
+                  index  === VACATION_DURATION - diffFormatted ? "lightblue" : "blue"
+                }`,
+                backgroundColor: `${
+                  index  < VACATION_DURATION - diffFormatted ? "#827b7b" : "blue"
+                }`,
+                color: '#fff',
+                textAlign: 'center',
+                lineHeight: '45px',
+                margin: '5px auto'
+              }}
+            >
+              {index + 1}
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
